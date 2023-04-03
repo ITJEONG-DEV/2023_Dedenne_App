05539e4023ee8a3c50ee5761f2e9bc960d3e689e
ITJEONG-DEV <derbana1027@gmail.com>
2023-04-03 14:18:36 +0900

    트레이 기능 추가
    1. 트레이 아이콘에 애니메이션 설정
    2. 트레이 메뉴 추가
      - 앱 열기
      - 앱 종료
    3. 트레이 아이콘 더블 클릭 이벤트 추가 ( 앱 열기 )

--- Reference.md
+++ Reference.md
@@ -2,7 +2,7 @@
 
 - Vite, electron, ts 환경 설정
     - [Vite + Vue 3 + electron + TypeScript](https://dev.to/brojenuel/vite-vue-3-electron-5h4o)
-
+<br>
 - ESModule / CommonJS 지원 관련 오류
     - package.json > "type": "module" 삭제
 
--- electron/electron.js
+++ electron/electron.js
@@ -1,23 +1,26 @@
 // electron/electron.js
 const path = require('path');
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, Menu, Tray } = require('electron');
 
 const isDev = process.env.IS_DEV == "true" ? true : false;
 
-function createWindow() {
+let mainWindow, tray, trayMenu;
+let trayIndex = 0, trayAnimationId = null
+
+const createWindow = () => {
   // Create the browser window.
-  const mainWindow = new BrowserWindow({
+  mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
       nodeIntegration: true,
-    },
-});
+    }
+  });
 
-// and load the index.html of the app.
-// win.loadFile("index.html");
-mainWindow.loadURL(
+  // and load the index.html of the app.
+  // win.loadFile("index.html");
+  mainWindow.loadURL(
     isDev
       ? 'http://localhost:3000'
       : `file://${path.join(__dirname, '../dist/index.html')}`
@@ -26,17 +29,59 @@ mainWindow.loadURL(
   if (isDev) {
     mainWindow.webContents.openDevTools();
   }
+
+  initTrayIconMenu();
+  setTrayIconAnimation();
+}
+
+const initTrayIconMenu = () => {
+  tray = new Tray(path.join(__dirname, "..", '/images/icon_.png'));
+  trayMenu = Menu.buildFromTemplate([
+    { label: '앱 열기', type: 'normal', click: () => { openApp(); }},
+    { label: '앱 종료', type: 'normal', click: () => { closeApp(); }}
+  ])
+  tray.setToolTip('DedenneApp');
+  tray.setContextMenu(trayMenu);
+
+  tray.addListener('double-click', () => {
+    openApp();
+  })
+}
+
+const openApp = () => {
+  mainWindow.show();
+}
+
+const closeApp = () => {
+  mainWindow.close();
+}
+
+const setTrayIconAnimation = (delay=200) => {
+  trayAnimationId = setInterval(() => {
+    if(trayIndex >= 10) {
+      trayIndex = trayIndex % 10;
+    }
+    tray.setImage(path.join(__dirname, "..", '/images/image_' + trayIndex + '.png'));
+    trayIndex +=1;
+  }, delay)
+}
+
+const stopTrayIconAnimation = () => {
+  if(trayAnimationId != null) { 
+    clearInterval(trayAnimationId)
+  }
+  trayAnimationId = null
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
-  createWindow()
+  createWindow();
   app.on('activate', function () {
     // On macOS it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
   })
 });
 
@@ -45,6 +90,7 @@ app.whenReady().then(() => {
 // explicitly with Cmd + Q.
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
+    stopTrayIconAnimation();
     app.quit();
   }
 });
