dc79ed1ab4bbe9cd210a62ad61b54e02a19597f6
ITJEONG-DEV <derbana1027@gmail.com>
2023-04-18 17:46:51 +0900

    각인서 v-avatar로 표시

--- src/components/CharacterEngraving.vue
+++ src/components/CharacterEngraving.vue
@@ -1,60 +1,41 @@
 <template>
   <v-sheet>
     <v-list
-      class="d-flex"
+      class="d-flex mt-1"
+      width="300px"
     >
       <v-list-item
-        class="justify-center"
         v-for="item in props.engraving?.Engravings"
         :key="item.Name"
       >
-        <v-img
-          :src="item.Icon"
-          width="32px"
-          height="32px"
-        ></v-img>
+        <template v-slot:prepend>
+          <v-avatar
+            width="32px"
+            height="32px"
+            class="mr-2"
+            style="outline: auto;"
+          >
+            <v-img
+              :src="item.Icon"
+            ></v-img>
+          </v-avatar>
+        </template>
         <v-list-item-subtitle>{{ item.Name }}</v-list-item-subtitle>
       </v-list-item>
     </v-list>
 
-    <template v-if="props.engraving != null && props.engraving.Effects.length > 6">
-      <v-list
-        class="d-flex pa-0"
-      >
-        <v-list-item
-          class="justify-center"
-          v-for="item in props.engraving?.Effects.slice(0, props.engraving?.Effects.length/2)"
-          :key="item.Name"
-        >
-          <v-list-item-subtitle>{{ item.Name }}</v-list-item-subtitle>
-        </v-list-item>
-      </v-list>
-      <v-list
-        class="d-flex pa-0"
-      >
-        <v-list-item
-          class="justify-center"
-          v-for="item in props.engraving?.Effects.slice(props.engraving?.Effects.length/2, )"
-          :key="item.Name"
-        >
-          <v-list-item-subtitle>{{ item.Name }}</v-list-item-subtitle>
-        </v-list-item>
-      </v-list>
-    </template>
 
-    <template v-if="props.engraving != null && props.engraving.Effects.length <= 6">
-      <v-list
-        class="d-flex"
+    <v-list
+      class="d-flex"
+    >
+      <v-list-item
+        class="justify-center"
+        v-for="item in props.engraving?.Effects"
+        :key="item.Name"
       >
-        <v-list-item
-          class="justify-center"
-          v-for="item in props.engraving?.Effects"
-          :key="item.Name"
-        >
-          <v-list-item-subtitle>{{ item.Name }}</v-list-item-subtitle>
-        </v-list-item>
-      </v-list>
-    </template>
+        <v-list-item-subtitle>{{ item.Name }}</v-list-item-subtitle>
+      </v-list-item>
+    </v-list>
 
     
 
